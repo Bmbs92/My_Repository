20e487553b776165e39b6e15a770362cf37c6167
braulioB berlanga <braulioberlanga@gmail.com>
2020-06-09 15:09:57 -0500

    Commit_3

--- Phyton/Learning_Python.py
+++ Phyton/Learning_Python.py
@@ -6,4 +6,86 @@
 'Hello'
 """Hello"""
 '''Hello'''
-print(Hello) 'this is a print'
+print("Hello")
+
+#Types of inputs
+
+#Strings:leters
+"Hello"
+print("Hello")
+print("2")
+print("Hello"+" "+"you")
+print("Hello"+" "+"2"+" "+"you")
+
+#Integer:numbers
+"2"
+"3"
+print(2)
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+print(3//2)
+print(3**2)
+
+#Float:decimals
+"1.23456"
+print(1.2345+2.3456)
+
+#Booleans:True or False
+True
+False
+"True=1"
+print(True*1)
+"False=0"
+print(False*1)
+print(True+True+False+False)
+
+#Variables
+"=, is exclusive for making variables"
+
+#Variables with strings
+a="Hello"
+b="you"
+print(a+" "+b)
+
+#Varibles with integer
+a=2
+b=3
+print(a+b)
+
+#Operations
+"==, means values are equal"
+">, means the values is greater than"
+"<, means that value is less than"
+">=, means the value is greater or equal than"
+"<=, means tha value is less or equla than"
+"!=, means values are not equal"
+
+#Operations and Booleans
+a=10
+b=20
+"True statement"
+print(b>a)
+"False statement"
+print(a==b)
+
+#Operations, Booleans and Variables
+x=b>a
+y=a==b
+print(x+y+y)
+
+#Logical operators
+"and, statement x and stament z are true"
+"or, statement x or z are true"
+"not, statement x is not true"
+
+"Statement x and statement y is True"
+print(x and y)
+"Statement x or statement y is True "
+print(x or y)
+"Is not True that statement x and statement y are True"
+print(not(x and y))
+"Is not True that either statement x or statement y is True"
+print(not(x or y))
+
